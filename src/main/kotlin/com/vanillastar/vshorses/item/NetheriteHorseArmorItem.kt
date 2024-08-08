a93e29d7327d96a9f8b7b792d1a7fb833f575c1e
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-08-08 03:08:10 -0700

    Add netherite horse armor (#9)
    Add netherite horse armor, and refactor horseshoe entity texture to a
    vanilla-consistent path

---
+++ src/main/kotlin/com/vanillastar/vshorses/item/NetheriteHorseArmorItem.kt
@@ -0,0 +1,24 @@
+package com.vanillastar.vshorses.item
+
+import com.vanillastar.vshorses.utils.getModIdentifier
+import net.minecraft.item.AnimalArmorItem
+import net.minecraft.item.ArmorItem
+import net.minecraft.item.ArmorMaterials
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemGroups
+import net.minecraft.registry.RegistryKey
+
+abstract class NetheriteHorseArmorItem: ModItem, AnimalArmorItem(
+  ArmorMaterials.NETHERITE, Type.EQUESTRIAN, false,
+  // Same as netherite armor.
+  Settings().maxDamage(ArmorItem.Type.BODY.getMaxDamage(37))
+) {
+  override val id = getModIdentifier("netherite_horse_armor")
+  override val itemGroup: RegistryKey<ItemGroup> = ItemGroups.COMBAT
+}
+
+@JvmField
+val NETHERITE_HORSE_ARMOR_ITEM = object: NetheriteHorseArmorItem() {
+  override fun getEntityTexture() =
+    getModIdentifier("textures/entity/horse/armor/horse_armor_netherite.png")
+}
