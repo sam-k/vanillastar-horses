1664550a565c78ab729ab226613e14d9165524cd
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-08-08 03:07:38 -0700

    Durability

--- src/main/kotlin/com/vanillastar/vshorses/item/NetheriteHorseArmorItem.kt
+++ src/main/kotlin/com/vanillastar/vshorses/item/NetheriteHorseArmorItem.kt
@@ -2,13 +2,16 @@ package com.vanillastar.vshorses.item
 
 import com.vanillastar.vshorses.utils.getModIdentifier
 import net.minecraft.item.AnimalArmorItem
+import net.minecraft.item.ArmorItem
 import net.minecraft.item.ArmorMaterials
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemGroups
 import net.minecraft.registry.RegistryKey
 
 abstract class NetheriteHorseArmorItem: ModItem, AnimalArmorItem(
-  ArmorMaterials.NETHERITE, Type.EQUESTRIAN, false, Settings().maxCount(1)
+  ArmorMaterials.NETHERITE, Type.EQUESTRIAN, false,
+  // Same as netherite armor.
+  Settings().maxDamage(ArmorItem.Type.BODY.getMaxDamage(37))
 ) {
   override val id = getModIdentifier("netherite_horse_armor")
   override val itemGroup: RegistryKey<ItemGroup> = ItemGroups.COMBAT
