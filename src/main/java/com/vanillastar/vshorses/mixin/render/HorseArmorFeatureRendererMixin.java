378f7f58ca425c63e380bb8903f7ddbaa07d165b
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-08-17 08:11:17 -0700

    Render horse armor trims (#15)
    Render trims on horse armor.

--- src/main/java/com/vanillastar/vshorses/mixin/render/HorseArmorFeatureRendererMixin.java
+++ src/main/java/com/vanillastar/vshorses/mixin/render/HorseArmorFeatureRendererMixin.java
@@ -3,8 +3,10 @@ package com.vanillastar.vshorses.mixin.render;
 import com.vanillastar.vshorses.render.HorseArmorEntityModel;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
@@ -12,20 +14,48 @@ import net.minecraft.client.render.entity.feature.HorseArmorFeatureRenderer;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.client.render.entity.model.EntityModelLoader;
 import net.minecraft.client.render.entity.model.HorseEntityModel;
+import net.minecraft.client.render.model.BakedModelManager;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.item.AnimalArmorItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.trim.ArmorTrim;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.function.Function;
+
+import static com.vanillastar.vshorses.render.HorseArmorTrimsRenderLayerKt.HORSE_ARMOR_TRIMS_ATLAS_TEXTURE;
+import static com.vanillastar.vshorses.utils.IdentiferHelperKt.getModIdentifier;
+
 @Mixin(HorseArmorFeatureRenderer.class)
 @Environment(EnvType.CLIENT)
 public abstract class HorseArmorFeatureRendererMixin extends FeatureRenderer<HorseEntity, HorseEntityModel<HorseEntity>> {
+  @Unique
+  private static final BakedModelManager MODEL_MANAGER =
+    MinecraftClient.getInstance().getBakedModelManager();
+
+  @Unique
+  private static final Function<ArmorTrim, Identifier> TRIM_MODEL_ID_GETTER =
+    Util.memoize(trim -> getModIdentifier(String.format(
+      "trims/models/horse_armor/%s_%s",
+      trim.getPattern().value().assetId().getPath(),
+      trim.getMaterial().value().assetName()
+    )));
+
   @Shadow
   @Final
   @Mutable
@@ -38,6 +68,46 @@ public abstract class HorseArmorFeatureRendererMixin extends FeatureRenderer<Hor
     super(context);
     this.model =
       new HorseArmorEntityModel<>(loader.getModelPart(EntityModelLayers.HORSE_ARMOR));
+  }
+
+  @Unique
+  private void renderTrim(
+    @NotNull ArmorTrim trim,
+    MatrixStack matrices,
+    @NotNull VertexConsumerProvider vertexConsumers,
+    int light
+  ) {
+    @Nullable SpriteAtlasTexture horseArmorTrimsAtlas =
+      MODEL_MANAGER.getAtlas(HORSE_ARMOR_TRIMS_ATLAS_TEXTURE);
+    if (horseArmorTrimsAtlas == null) {
+      return;
+    }
+
+    Sprite sprite =
+      horseArmorTrimsAtlas.getSprite(TRIM_MODEL_ID_GETTER.apply(trim));
+    VertexConsumer vertexConsumer =
+      vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(
+        HORSE_ARMOR_TRIMS_ATLAS_TEXTURE));
+    this.model.render(
+      matrices,
+      sprite.getTextureSpecificVertexConsumer(vertexConsumer),
+      light,
+      OverlayTexture.DEFAULT_UV
+    );
+  }
+
+  @Unique
+  private void renderGlint(
+    MatrixStack matrices,
+    @NotNull VertexConsumerProvider vertexConsumers,
+    int light
+  ) {
+    this.model.render(
+      matrices,
+      vertexConsumers.getBuffer(RenderLayer.getEntityGlint()),
+      light,
+      OverlayTexture.DEFAULT_UV
+    );
   }
 
   @Inject(
@@ -61,14 +131,20 @@ public abstract class HorseArmorFeatureRendererMixin extends FeatureRenderer<Hor
     float headPitch,
     CallbackInfo ci
   ) {
+    ItemStack itemStack = horseEntity.getBodyArmor();
+    if (!(itemStack.getItem() instanceof AnimalArmorItem animalArmorItem) ||
+      animalArmorItem.getType() != AnimalArmorItem.Type.EQUESTRIAN) {
+      return;
+    }
+
+    ArmorTrim armorTrim = itemStack.get(DataComponentTypes.TRIM);
+    if (armorTrim != null) {
+      this.renderTrim(armorTrim, matrices, vertexConsumers, light);
+    }
+
     // This fixes https://bugs.mojang.com/browse/MC-16829.
     if (horseEntity.getBodyArmor().hasGlint()) {
-      this.model.render(
-        matrices,
-        vertexConsumers.getBuffer(RenderLayer.getEntityGlint()),
-        light,
-        OverlayTexture.DEFAULT_UV
-      );
+      this.renderGlint(matrices, vertexConsumers, light);
     }
   }
 }
