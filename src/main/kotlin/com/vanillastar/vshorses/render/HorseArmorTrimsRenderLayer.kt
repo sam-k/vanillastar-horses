cdea375f2f72c67961186e8ea1c45c43be76f555
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-08-18 20:11:20 -0700

    Implement

--- src/main/kotlin/com/vanillastar/vshorses/render/HorseArmorTrimsRenderLayer.kt
+++
@@ -1,8 +0,0 @@
-package com.vanillastar.vshorses.render
-
-import com.vanillastar.vshorses.utils.getModIdentifier
-
-/** Identifier for the generated atlas texture file for horse armor trims. */
-@JvmField
-val HORSE_ARMOR_TRIMS_ATLAS_TEXTURE =
-  getModIdentifier("textures/atlas/horse_armor_trims.png")
---
+++ src/main/kotlin/com/vanillastar/vshorses/render/TextureAtlasHelper.kt
@@ -0,0 +1,11 @@
+package com.vanillastar.vshorses.render
+
+import com.vanillastar.vshorses.utils.getModIdentifier
+import net.minecraft.util.Identifier
+
+/**
+ * Builds the ID for the generated texture atlas file corresponding to the given
+ * atlas ID.
+ */
+fun getTextureAtlasId(atlasId: Identifier) =
+  getModIdentifier("textures/atlas/${atlasId.path}.png")
